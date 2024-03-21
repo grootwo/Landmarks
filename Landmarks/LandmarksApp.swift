e422f4280716f1311e00fa72da1f1d798a593ec1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-21 12:02:29 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 임휘도 on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
