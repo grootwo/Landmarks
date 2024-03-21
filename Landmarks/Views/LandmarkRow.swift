cb0c1f967e2b3f2e5f3e48ed65ece43701518ec9
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-21 21:48:09 +0900

    Make Landmark Model

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Groo on 3/21/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
