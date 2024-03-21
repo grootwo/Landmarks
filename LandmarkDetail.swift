4c82465df7e9980a9de1884de01862a07f40d2b1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-21 23:21:29 +0900

    Pass data into LandmarkDetail

--- LandmarkDetail.swift
+++ LandmarkDetail.swift
@@ -8,40 +8,46 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 200)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130) // 이거 왜 넣은 걸까? 천장에서 지도가 아래로 좀 이동함
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                
+        ScrollView {
+            VStack {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .frame(height: 200)
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130) // 이거 왜 넣은 걸까? 천장에서 지도가 아래로 좀 이동함
                 VStack(alignment: .leading) {
-                    Text("About Turtle Rock")
-                        .font(.headline)
-                    Text("Descriptive text goes here.")
+                    Text(landmark.name)
+                        .font(.title)
+                    HStack {
+                        Text(landmark.park)
+                            .font(.subheadline)
+                        Spacer()
+                        Text(landmark.state)
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("About \(landmark.name)")
+                            .font(.headline)
+                        Text(landmark.description)
+                    }
                 }
+                .padding() // 존재 이유를 잘 모르겠음
+                Spacer()
             }
-            .padding() // 존재 이유를 잘 모르겠음
-            Spacer()
+            .navigationTitle(landmark.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
 }
